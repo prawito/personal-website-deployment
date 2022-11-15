892d6a81c406c18a8d85adaacefaa27952d9158e
Prawito Hudoro <prawitohudoro@gmail.com>
2022-11-15 10:03:43 +0700

    ready to deploy

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
+import photo from './pawito.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={photo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to my personal website
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://youtube.com/@prawitohudoro"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Programming With Me
         </a>
       </header>
     </div>
